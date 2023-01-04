d48ce3379c9d39243cfce0972954d04f6fc7f43d
Petar Lenart <petar.lenart@gmail.com>
2023-01-04 17:19:42 +0100

    added image to HomeScreen if no boards are available

--- app/src/main/java/com/plenart/organizeme_compose/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/plenart/organizeme_compose/ui/homeScreen/HomeScreen.kt
@@ -1,9 +1,7 @@
 package com.plenart.organizeme_compose.ui.homeScreen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -14,10 +12,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.plenart.organizeme_compose.R
 import com.plenart.organizeme_compose.ui.components.ItemBoard
 import com.plenart.organizeme_compose.ui.components.ItemBoardViewState
+import com.plenart.organizeme_compose.ui.theme.IntroDescription
+import com.plenart.organizeme_compose.ui.theme.IntroHeroText
+import com.plenart.organizeme_compose.ui.theme.LocalSpacing
 
 @Composable
 fun HomeScreenRoute(
@@ -67,24 +70,54 @@ fun HomeScreen(
         Button(onClick = { onButtonAction() }) {
             Text(text = "create board")
         }
-        LazyColumn {
-            items(
-                items = viewState.boards,
-                key = { board ->
-                    board.boardId
+        if (viewState.boards.isEmpty()) {
+            NoBoardsAssigned()
+        }
+        else{
+            LazyColumn {
+                items(
+                    items = viewState.boards,
+                    key = { board ->
+                        board.boardId
+                    }
+                ) { board ->
+                    ItemBoard(
+                        viewState = ItemBoardViewState(
+                            boardId = board.boardId,
+                            boardName = board.boardName,
+                            createdBy = board.createdBy
+                        ),
+                        onBoardAction = { onBoardAction(board.boardId) }
+                    )
                 }
-            ) { board ->
-                ItemBoard(
-                    viewState = ItemBoardViewState(
-                        boardId = board.boardId,
-                        boardName = board.boardName,
-                        createdBy = board.createdBy
-                    ),
-                    onBoardAction = { onBoardAction(board.boardId) }
-                )
             }
         }
     }
+}
+
+@Composable
+fun NoBoardsAssigned(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.checklist_purple_transparent),
+        contentDescription = stringResource(id = R.string.no_boards_available),
+        modifier = modifier.padding(LocalSpacing.current.medium)
+    )
+    Spacer(modifier = Modifier.height(LocalSpacing.current.medium))
+    Text(
+        text = stringResource(id = R.string.no_boards_available),
+        style = IntroHeroText,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier.padding(
+            vertical = LocalSpacing.current.small
+        )
+    )
+    Text(
+        text = stringResource(id = R.string.create_board_description),
+        style = IntroDescription,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
 }
 
 @Preview(showBackground = true)
